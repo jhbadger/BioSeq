a7e1365238e8bc2691e2a6f4952351943fa452c6
Jonathan Badger <jhbadger@gmail.com>
2016-08-15 16:37:09 -0400

    line length option in reformatFasta.cr

--- examples/reformatFasta.cr
+++ examples/reformatFasta.cr
@@ -1,16 +1,16 @@
-#!/usr/bin/env ruby
-
 require "../src/Bioseq"
 require "option_parser"
 
 input = ""
 header = false
+len = 60
 
 OptionParser.parse! do |parser|
   parser.banner = "Usage: reformatFasta [options] --input <file>"
   parser.on("-i FILE", "--input=FILE", "Specifies input file") { |f| input = f }
   parser.on("-h", "--header", "keep sequence header") { header = true }
 
+  parser.on("-l len", "--length=len", "Specifies line length (default 60)") { |l| len = l }
   parser.on("-e", "--help", "Show this help") { puts parser }
 end
 
@@ -18,6 +18,7 @@ if input == ""
   STDERR.printf("reformatFasta: --input file is required\n")
 else
   BioSeq::FastaFile.new(input).each do |seq|
-    print seq.to_fasta
+    seq.definition = seq.entry_id if header
+    print seq.to_fasta(len: len)
   end
 end
