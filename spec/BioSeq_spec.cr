66c9a97120a7657ac38b86be5365fc1879185475
Jonathan Badger <jhbadger@gmail.com>
2017-03-21 14:41:14 -0400

    Fastq refactoring

--- spec/BioSeq_spec.cr
+++ spec/BioSeq_spec.cr
@@ -1,19 +1,12 @@
 require "./spec_helper"
 
 describe BioSeq do
-  s = BioSeq::Nucleic.new("Foobar gene","AAATTAAGGGGA")
-  pep = BioSeq::Protein.new("Qux protein","MTCSSWLPRRD")
+  s = BioSeq::Sequence.new("Foobar gene","AAATTAAGGGGA")
   it "entry_id is first word of definition" do
     s.entry_id.should eq("Foobar")
   end
   it "length should be 12" do
     s.size.should eq(12)
-  end
-  it "type of s is Nucleic" do
-    s.type.should eq(BioSeq::Nucleic)
-  end
-  it "type of pep is Protein" do
-    pep.type.should eq(BioSeq::Protein)
   end
   it "should print as fasta" do
     s.to_fasta.should eq(">Foobar gene\nAAATTAAGGGGA\n")
@@ -25,7 +18,7 @@ describe BioSeq do
     s.to_fasta("Baz").should eq(">Baz\nAAATTAAGGGGA\n")
   end
   it "should allow seq to be updated" do
-    f = BioSeq::Nucleic.new("Foo","GGG")
+    f = BioSeq::Sequence.new("Foo","GGG")
     f.seq = "AAA"
     f.to_fasta.should eq(">Foo\nAAA\n")
   end
@@ -39,6 +32,38 @@ describe BioSeq do
     s.gc_percent.should eq(33)
   end
   it "should translate correctly" do
-    s.translate.should eq("K")
+    s.translate.seq.should eq("KLRG")
   end
+  it "should load the first and second Fasta sequences" do
+     f = File.new("seq_tmp.fa","w")
+     f.print(">seq1\nAAA\n>seq2\nTTT\n")
+     f.close
+     seq_iter = BioSeq::FastxFile.new("seq_tmp.fa") 
+     s1 = seq_iter.next
+     if !s1.nil?
+       s1.seq.should eq("AAA")
+     end
+     s2 = seq_iter.next
+     if !s2.nil?
+       s2.seq.should eq("TTT")
+     end
+     File.delete("seq_tmp.fa")
+  end
+  it "should load the first and second Fastq sequences" do
+     f = File.new("seq_tmp.fq","w")
+     f.print("@seq1\nAAA\n+\n\HHH\n@seq2\nTTT\n+\nHHH\n")
+     f.close
+     seq_iter = BioSeq::FastxFile.new("seq_tmp.fq", fastq=true) 
+     s1 = seq_iter.next
+     if !s1.nil?
+       s1.seq.should eq("AAA")
+       s1.qual.should eq("HHH")
+     end
+     s2 = seq_iter.next
+     if !s2.nil?
+       s2.seq.should eq("TTT")
+     end
+     File.delete("seq_tmp.fq")
+  end
+  
 end
--- src/BioSeq.cr
+++ src/BioSeq.cr
@@ -6,14 +6,16 @@ module BioSeq
   class Sequence
     getter entry_id
     getter seq
+    getter qual
     getter definition
     setter seq
+    setter qual
     setter definition
     def initialize(definition, seq, qual = "")
       @entry_id = ""
       @definition = ""
       @seq = ""
-      @qual = ""
+      @qual = qual
       @entry_id = definition.split(" ").first
       @definition = definition
       @seq = seq
@@ -21,23 +23,14 @@ module BioSeq
     def size
       @seq.size
     end
-    def type
-      if @seq.count("ATGCRN") > @seq.size*0.9
-        Nucleic
-      else
-        Protein
-      end
-    end
     def to_fasta(header=@definition, len=60)
       ">"+header+"\n"+@seq.gsub(Regex.new(".{1,#{len}}"), "\\0\n").chomp+"\n"
     end
-  end
-  class Nucleic < Sequence
     def to_RNA
-      Nucleic.new(@definition, @seq.gsub("T","U"))
+      Sequence.new(@definition, @seq.gsub("T","U"))
     end
     def to_DNA
-      Nucleic.new(@definition, @seq.gsub("U","T"))
+      Sequence.new(@definition, @seq.gsub("U","T"))
     end
     def gc_percent
       100*(@seq.count("GC"))/@seq.size
@@ -54,25 +47,21 @@ module BioSeq
                 "GGG"]
       pseq = ""
       @seq.upcase.gsub("U","T").chars.each_slice(3) do |c|
-        aanum = codons.index c
+        aanum = codons.index c.join
         if aanum
           pseq += codes[code - 1][aanum]
         else
           pseq += "X"
         end
       end
-      Protein.new(@definition, pseq)
+      Sequence.new(@definition, pseq)
     end
-  end
-  class Fastq < Nucleic
     def to_fastq(header=@definition)
       "@"+header+"\n"+@seq+"\n"+"+\n"+@qual+"\n"
     end
   end
-  class Protein < Sequence
-  end
   class FastxFile
-    def initialize(filename, type=Sequence)
+    def initialize(filename, fastq = false)
       if filename.includes?(".gz")
         @file = Gzip::Reader.new(filename)
       elsif filename.includes?(".bz2")
@@ -80,48 +69,35 @@ module BioSeq
       else
         @file = File.new(filename)
       end
-      @type = type
+      @fastq = fastq
+      @line_count = 0
     end
     def next
-      each(limit=1) do |seq|
+      each do |seq|
         return seq
       end
     end
-    def each(limit=nil)
+    def each
       seq = ""
       header = ""
-      count = 0
-      if @type != Fastq
-        @file.each_line do |line|
-          if line[0] == '>'
-            if seq != ""
-              @type = Sequence.new(header, seq, "").type if @type==Sequence
-              yield @type.new(header, seq, "")
-              seq = ""
-              count += 1
-              break if limit && count == limit
-              header = line[1..line.size].chomp
-            end
-          else
-            seq += line.chomp.upcase
-          end
+      qual = ""
+      @file.each_line do |line|
+        if line[0] == '>' || (@line_count % 4 == 0 && @fastq)
+          if (!@fastq && seq != "") || (@fastq && qual != "")
+            @line_count += 1
+            yield Sequence.new(header, seq, qual)
+            seq = ""
+            qual = ""
+	  end
+          header = line[1..line.size].chomp
+        elsif (@fastq && @line_count % 4 == 1) || !@fastq
+          seq += line.chomp.upcase
+        elsif (@fastq && @line_count % 4 == 3)
+          qual += line.chomp.upcase
         end
-        yield @type.new(header, seq, "") if seq != ""
-      else
-        count = 0
-        qual = ""
-        @file.each_line do |line|
-          lnum = count % 4
-          if lnum == 0
-            header = line[1..line.size].chomp
-          elsif lnum == 1
-            seq = line.chomp
-          elsif lnum == 3
-            yield Fastq.new(header, seq, line) if seq != ""
-          end
-          count += 1
-        end
+        @line_count += 1
       end
+      yield Sequence.new(header, seq, qual) if seq != ""
     end
   end
 end
